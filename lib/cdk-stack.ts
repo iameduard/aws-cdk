2c2334f27381618aa2b666732a18c9dbc7abfc14
iameduard <iameduard@hotmail.com>
2024-08-05 11:12:00 -0300

    test LambdaQueryAthena y S3GlaciertoStandar

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -8,8 +8,8 @@ import * as events from 'aws-cdk-lib/aws-events';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -77,7 +77,53 @@ export class CdkStack extends cdk.Stack {
     });
 
 
+    _service = 'lambda';
+    _description = 'query_athena_base';
+    const _lambda2 = `${_environment}-${_region}-${_service}-${_description}`; //dev-us-east-2-lambda-query_athena_base
+    console.log(_lambda2)
 
+    /*
+    // define lambda function for Athena query
+    const queryAthenaBaseLambda = new nodejs.NodejsFunction(this, _lambda2, {
+      tracing: lambda.Tracing.ACTIVE,
+      handler: 'handler',
+      environment: {
+        REGION_NAME: 'us-east-2',
+        OUTPUT_LOCATION: `s3://${_bucket}/athena-output-results/`
+      },
+    });
+    */
+
+    const LambdaQueryAthenaBase = new NodejsFunction(this, "LambdaQueryAthenaBase", {
+      tracing: lambda.Tracing.ACTIVE,
+      environment: {
+        REGION_NAME: 'us-east-2',
+        OUTPUT_LOCATION: `s3://${_bucket}/athena-output-results/`
+      },
+    })
+
+
+    /*
+
+    const fn = new lambda.Function(this, 'S3EventNotificationsLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      functionName: 'S3EventNotificationsManager',
+      handler: 'manage-s3-event-notifications.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      reservedConcurrentExecutions: 1,
+      timeout: cdk.Duration.seconds(300)
+    });
+
+    */
+
+    new lambda.Function(this, 'SimpleLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,  // Usa la versión de Node.js que prefieras
+      functionName: 'SimpleLambdaFunction',
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/SimpleLambdaFunction')),  // Ruta al directorio que contiene el código
+    });
+
+  
     //Creación de la lambda que cambia el estado de los archivos S3 dentro del Bucket.
 
     _service = 'lambda';
@@ -85,38 +131,24 @@ export class CdkStack extends cdk.Stack {
     const _lambda = `${_environment}-${_region}-${_service}-${_description}`;
 
     const lambdaFunction = new lambda.Function(this, 'S3GlacierToStandardHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: `${_lambda}.handler`,  //dev-us-east2-lambda-changestate
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      runtime: lambda.Runtime.NODEJS_20_X,
+      functionName: 'S3GlacierToStandardHandler',
+      handler: 'index.handler',  //dev-us-east2-lambda-changestate
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/S3GlacierToStandardHandler')),
       environment: {
         BUCKET_NAME: bucket.bucketName,
       },
     });
 
+  /***********************************
 
-    /***********************************
-
-    _service = 'lambda';
-    _description = 'query_athena_base';
-    const _lambda2 = `${_environment}-${_region}-${_service}-${_description}`;
-
-    // define lambda function for Athena query
-    const queryAthenaBaseLambda = new nodejs.NodejsFunction(this, "queryAthenaBaseFunction", {
-      tracing: lambda.Tracing.ACTIVE,
-      entry: `${_lambda2}.ts`,  // Adjust this path to your actual file location
-      handler: 'handler',
-      environment: {
-        REGION_NAME: 'us-east-2',
-        OUTPUT_LOCATION: `s3://${_bucket}/athena-output-results/`
-      },
-    });
 
     //Grant permiso a la lambda para leer y escribir en bucket S3..
 
-    bucket.grantReadWrite(lambdaFunction);
+    bucket.grantReadWrite(LambdaQueryAthenaBase);
 
     //Define apigateway
-    const api = new apigateway.RestApi(this, "RestAPI", {
+    const api = new apigateway.RestApi(this, "devHistRestAPI", {
       deployOptions: {
         dataTraceEnabled: true,
         tracingEnabled: true
@@ -124,10 +156,10 @@ export class CdkStack extends cdk.Stack {
     })
 
     //Define POST endpoint and associate it with queryAthenaBase lambda
-    const postEndpoint = api.root.addResource("execute_query");
-    postEndpoint.addMethod("POST", new apigateway.LambdaIntegration(queryAthenaBaseLambda));
-  
-    ************************************/
+    const executeQueryEndpoint = api.root.addResource("query");
+    const executeQueryEndpointMethod = executeQueryEndpoint.addMethod("POST", new apigateway.LambdaIntegration(LambdaQueryAthenaBase));
+
+    */
    
   }
 }
